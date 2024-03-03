0a88d580ec2f15cc7fdd068ed15d6aa3bb50e951
[Muhammed Hisham] <[muhammedhisham2003@gmail.com]>
2024-03-03 12:38:34 +0530

    firebase hosting done

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_JtioExsHCmex9Tq9ARiyehNaUCfK9do",
+  authDomain: "kasol-krusade.firebaseapp.com",
+  projectId: "kasol-krusade",
+  storageBucket: "kasol-krusade.appspot.com",
+  messagingSenderId: "1037305692644",
+  appId: "1:1037305692644:web:255625486df32c2b7787f1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
